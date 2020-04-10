a70f289748988b1a03bc55510f380a05648ea37b
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-04-10 15:12:39 -0400

    progress push

--- script.js
+++ script.js
@@ -1,148 +1,87 @@
 // SELECTING PARTS OF PAGE
-var title=document.getElementById("title");
-var timer=document.getElementById("timer");
+// div elements
+var timerDisplay=document.getElementById("timerDisplay");
+var titleDisplay=document.getElementById("titleDisplay");
 var questionDisplay=document.getElementById("questionDisplay");
 var answerDisplay=document.getElementById("answerDisplay");
-var resultDisplay=document.getElementById("resultDisplay");
-// DIV CONTAINING BUTTONS FOR EVENT LISTENING
 var answerButtons=document.getElementById("answerButtons");
-// EVENT LISTENIG FOR BUTTONS
+var resultDisplay=document.getElementById("resultDisplay");
+
+// text inside div elements
+var timerText=document.getElementById("timerText");
+var titleText=document.getElementById("titleText");
+var questionText=document.getElementById("questionText");
+var answerText=document.getElementById("answerText");
+var resultText=document.getElementById("resultText");
+
+//SELECTING BUTTONS
 answerButtons.addEventListener("click",buttonClicked);
-//SELECTING BUTTONS 
 var button1=document.getElementById("butt1");
 var button2=document.getElementById("butt2");
 var button3=document.getElementById("butt3");
 var button4=document.getElementById("butt4");
-// USER SCORE
-var userScore=0;
-// TIMER LEFT
-var timeLeft=10;
-//TIMER 
-for(var i=0;i<10;i++){
-  setInterval(console.log("test"),10000);
-  setInterval(console.log("and"),10000);
+// FUNCTION TO IDENTIFY BUTTON
+function buttonClicked(){
+  switch(event.target){
+    case button1:
+    return "button1";
+    
+    
+    case button2:
+    return "button2";
+    
+    case button3:
+    return "button3";
+    
+    case button4:
+    return "button4";
+  }
+} 
+
+// 1. Welcome 
+function welcomeScreen(){
+  timerText.textContent="";
+  titleText.textContent="Welcome!";
+  questionText.textContent="Are you ready for a quiz?";
+  answerText.textContent="";
+  resultText.textContent="Welcome!";
   
 }
-function countdown() {
-  for (var i = 10; i > 0; i--) {
-    timeLeft--
-    document.querySelector("timerP").textContent=timeLeft;
-    console.log(timeLeft);
-  }
-};
-// FUNCTION TO IDENTIFY BUTTON
-function buttonClicked(){
-  console.log(event.target);
-  switch(event.target){
-    case button1:
-    console.log("button1");
-    break;
-    
-    case button2:
-    console.log("button2");
-    break;
-    
-    case button3:
-    console.log("button3");
-    break;
-    
-    case button4:
-    console.log("button4");
-    break;
-  }
-}
-// CONDENSING FUNCTION TO SET TITLE BLOCK
-function setTitleText(stringInput){
-  title.textContent=stringInput;
-}
-// CONDENSING FUNCTION TO SET QUESTION BLOCK
-function setQuestionText(stringInput){
-  questionText.textContent=stringInput;
-}
-
-
-
-
-
-
-
-// Welcome page with start button
-// Start button is clicked, timer starts, start questions loop
-// Display Question
-// Display possible answers in random buttons
-
-
-
-// 1. StartPage
-
-// Set title To Welcome
-// Set Question to describe quiz
-// Set Answers to have start button
-
-// FUNCTION TO ACT AS TITLE SCREEN
-function welcomeScreen(){
-  setTitleText("Welcome!");
-  setQuestionText("Hit start when you're ready");
-}
-
-
 
 // 2. Questions
-// FUNCTION TO DISPLAY CORRECT
-function gotAnswer(){
-  resultDisplay.textContent="Correct!"
-}
-// FUNCTION TO DISPLAY INCORRECT
-function missedAnswer(){
-  resultDisplay.textContent="Inorrect!"
-}
-// Set Title to display question number
-// question index in the array
-// Set Question to display Question text
-// questionNumber.q
-// Set Answers to display possible answers
-// random assign button to c,w1,w2,w3
-
-// FUNCTION TO SETUP QUESTION
 function questionScreen(){
-  // QUESTION AND ANSWERS
-  var questionArray = [
-    questionOne,
-    questionTwo,
-    questionThree,
-    questionFour
-  ]; 
+  timerText.textContent="Questions!";
+  titleText.textContent="Questions!";
+  questionText.textContent="Questions!";
+  answerText.textContent="Questions!";
+  resultText.textContent="Questions!";
 }
-// FUNCTION TO IDENTIFY BUTTON
-function buttonClicked(){
-  console.log(event.target);
-  switch(event.target){
-    case button1:
-    console.log("button1");
-    break;
-    
-    case button2:
-    console.log("button2");
-    break;
-    
-    case button3:
-    console.log("button3");
-    break;
-    
-    case button4:
-    console.log("button4");
-    break;
+// WHILE TIME LEFT OR QUESTIONS LEFT
+while(timerValue>0 || length.questionArray>0){
+  for (var i = 0; i < questionArray.length; i++){
+    var currentQuestion=questionArray[i];
+    var currentQuestionText=currentQuestion.q;
+    var currentAnswer=currentQuestion.c;
   }
 }
 
+
+// QUESTION AND ANSWERS
+var questionArray = [
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour
+]; 
+
+
 // 3. Score
-// Set the Title to display finished
-// Set the Quesiton to display score
-
-
-// FUNCTION TO ACT AS SCORE SCREEN
 function scoreScreen(){
-  title.textContent="SCORE:" 
+  timerText.textContent="Score!";
+  titleText.textContent="Score!";
+  questionText.textContent="Score!";
+  answerText.textContent="Score!";
+  resultText.textContent="Score!";
 }
 
 // QUESTIONS AND ANSWERS
@@ -174,5 +113,21 @@ var questionFour = {
   w2:"Dr. Doom",
   w3:"Dr. Who"
 }
+
+
+function randomizeButtons(choices){
+  rand1=Math.random();
+  rand2=Math.random();
+  rand3=Math.random();
+  rand4=Math.random();
+  
+    
+}
+
+
+// Loadpage
+// Call the welcome page which sets the title 
+
+
 
 
