eef036aff9ba3767d70df4d68ee899e6250aebad
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-04-14 21:29:29 -0400

    added a prompt as a welcome screen with the option to cancel, added some more questions, fixed some timer issues

--- script.js
+++ script.js
@@ -14,82 +14,105 @@ var questionText=document.getElementById("questionText");
 var answerText=document.getElementById("answerText");
 var resultText=document.getElementById("resultText");
 
-// CREATING THE TIMER
-var timeLeft;
-function timer(){ 
-  timeLeft=10;
-  timerText.textContent="Time left:"+timeLeft;
-  setInterval(
-    function(){
-      // IF TIME LEFT IS GREATER THAN 0 DECREMENT
-      if(timeLeft>0){
-        timeLeft--;
-      }
-      timerText.textContent="Time left:"+timeLeft;
-    },1000);
-    
-  }
-  
-  // USER SCORE, WHEN THEY GET A QUETION RIGHT INCREMENT
-  var score=0;
-  
-  // INDIVIDUAL QUESTIONS WITH A CORRECT AND 3 WRONG
-  var questionOne = {
-    q:"David Gilmour is famous for playing what kind of guitar?",
-    a: [
-      { n: "Fender Strat", correct: true }, 
-      { n: "Gibson SG", correct: false }, 
-      { n: "Gretsch DuoJet", correct: false }, 
-      { n: "BC Rich Warlock", correct: false }
-    ] 
-  }
-  var questionTwo = {
-    q:"The album Animals is about what?",
-    a:[
-      { n: "Capitalism", correct: true},
-      { n: "Women",correct: false},
-      { n: "Childhood Memories",correct: false},
-      { n: "Fluffy Animals", corect: false}
-    ]
-  }
-  var questionThree = {
-    q:"What concert incident inspired Roger Waters to write The Wall?",
-    a:[
-      { n: "Roger Spits on a Fan", correct: true},
-      { n: "Roger Highfives a Fan",correct: false},
-      { n: "Roger suffers a brain injury from a fall",correct: false},
-      { n: "Roger poops himself", corect: false}
-    ]
-  }
-  var questionFour = {
-    q:"The Album Artwork for Saucer full of secret features which superhero?",
-    a: [
-      {n:"Dr. Strange", correct: true},
-      {n:"Dr. Manhattan", correct: false},
-      {n:"Dr. Doom", correct: false},
-      {n:"Dr. Who", correct: false},
-    ]
-  }
-  // DELCLARING QUESTION ARRAY
-  var questionArray = [ questionOne, questionTwo, questionThree, questionFour ]; 
-  
-  
-  // INDEX OF THE CURRENT QUESTION
-  var currQuizIndex = 0;
-  var timeRemaining = 1000;
-  
-  
-  // FUNCTION TO CALL WHEN THE PAGE LOADS
-  function startQuiz(){
+
+// USER SCORE, WHEN THEY GET A QUETION RIGHT INCREMENT
+var score=0;
+
+// INDIVIDUAL QUESTIONS WITH A CORRECT AND 3 WRONG
+var questionOne = {
+  q:"David Gilmour is famous for playing what kind of guitar?",
+  a: [
+    { n: "Fender Strat", correct: true }, 
+    { n: "Gibson SG", correct: false }, 
+    { n: "Gretsch DuoJet", correct: false }, 
+    { n: "BC Rich Warlock", correct: false }
+  ] 
+}
+var questionTwo = {
+  q:"The album Animals is about what?",
+  a:[
+    { n: "Capitalism", correct: true},
+    { n: "Women",correct: false},
+    { n: "Childhood Memories",correct: false},
+    { n: "Fluffy Animals", corect: false}
+  ]
+}
+var questionThree = {
+  q:"What concert incident inspired Roger Waters to write The Wall?",
+  a:[
+    { n: "Roger Spits on a Fan", correct: true},
+    { n: "Roger Highfives a Fan",correct: false},
+    { n: "Roger suffers a brain injury from a fall",correct: false},
+    { n: "Roger poops himself", corect: false}
+  ]
+}
+var questionFour = {
+  q:"The Album Artwork for Saucer full of secret features which superhero?",
+  a: [
+    {n:"Dr. Strange", correct: true},
+    {n:"Dr. Manhattan", correct: false},
+    {n:"Dr. Doom", correct: false},
+    {n:"Dr. Who", correct: false},
+  ]
+}
+var questionFive = {
+  q:"Comfortably Numb is about what drug?",
+  a: [
+    {n:"Heroin", correct: true},
+    {n:"LSD", correct: false},
+    {n:"Cannabis", correct: false},
+    {n:"Alcohol", correct: false},
+  ]
+}
+var questionSix = {
+  q:"Only which member has been credited on every Pink Floyd album?",
+  a: [
+    {n:"Nick Mason", correct: true},
+    {n:"Roger Waters", correct: false},
+    {n:"David Gilmour", correct: false},
+    {n:"Richard Wright", correct: false},
+  ]
+}
+var questionSeven = {
+  q:"Which of these song titles is correct?",
+  a: [
+    {n:"Alan's Psychedelic Breakfast", correct: true},
+    {n:"Greg's Trippy Entre", correct: false},
+    {n:"Fred's Galactic Tea-Time", correct: false},
+    {n:"George's Gorgeous Porridge", correct: false},
+  ]
+}
+
+
+// DELCLARING QUESTION ARRAY
+var questionArray = [ questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven]; 
+
+
+// INDEX OF THE CURRENT QUESTION
+var currQuizIndex = 0;
+var timeRemaining = 200;
+
+
+// FUNCTION TO CALL WHEN THE PAGE LOADS
+function startQuiz(){
+  var ready=confirm("Are you ready for a quiz?");
+  if(ready===false){
+    titleText.textContent="Oh...";
+    questionText.textContent="Well, come back when you're ready!";
+  } 
+  else{
     var countdown = setInterval(function(){
       if( timeRemaining <= 0 ){
         scoreScreen();
         clearInterval(countdown);
-      }else
+      }else if(currQuizIndex===questionArray.length){
+        clearInterval(countdown);
+      }
+      else
       timeRemaining--;
       timerText.textContent="Time Remaining: "+ timeRemaining;
     }, 100 )
-    runQuiz();
+    runQuiz();}
   }
   
   // FUNCTION TO ASK QUESTIONS WHILE LEFT
@@ -98,6 +121,7 @@ function timer(){
     // IF CURRENT QUESTION INDEX IS LESS THAN ARRAY LENGTH, STILL QUIZ
     if( currQuizIndex < questionArray.length ){
       var currQuestion = questionArray[currQuizIndex];
+      titleText.textContent="Question: "+(currQuizIndex+1);
       renderQuestion(currQuestion);
     } 
     // ELSE END QUIZ
@@ -147,21 +171,15 @@ function timer(){
       // IF CORRECT ANSWER
       if(buttonValue==="yes"){
         score++;
-        resultText.textContent="Correct"
-        setTimeout(function(){
-          resultText.textContent=""
-        }),100  
+        resultText.textContent="Correct";  
       }
       
       // IF INCORRECT ANSWER
       if(buttonValue===null){
         timeRemaining-=20;
-        resultText.textContent="Wrong!"
-        setTimeout(function(){
-          resultText.textContent=""
-        }),100  
-        
+        resultText.textContent="Wrong!";        
       }
+
       // AFTER TESTING ANSWER VALIDITY
       // GO TO NEXT QUESTION
       currQuizIndex++;
@@ -174,9 +192,11 @@ function timer(){
   
   // 3. Score
   function scoreScreen(){
+    timerText.textContent="Time Remaining: 0"
     titleText.textContent="Finished!";
     questionText.textContent="Your score:";
-    answerDiv.textContent=score;
+    answerDiv.textContent=score+" of "+ questionArray.length;
+    resultText.textContent=Math.round(100*score/questionArray.length)+"%"
   }
   
   
